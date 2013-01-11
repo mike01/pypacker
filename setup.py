29c33ea7edae1d79e30e14c361392213512f3cf3
mike <michael.stahn@gmx.de>
2013-01-11 16:25:21 +0100

    configuration changes

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import dpkt
-# fix: https://code.google.com/p/dpkt/issues/attachmentText?id=63
+import pypacker
 import setuptools
 
-# fix: https://code.google.com/p/dpkt/issues/detail?id=82
-setup(name='dpkt',
-	version='1.1',
+setup(name='pypacker',
+	version='1.2',
 	author='Michael Stahn <michael.stahn@gmail.com>',
 	url='',
-	description='Fast, simple packet creation and parsing module',
-	packages=[ 'dpkt' ])
+	description='Fast and simple packet creation and parsing module',
+	packages=[ 'pypacker' ])
