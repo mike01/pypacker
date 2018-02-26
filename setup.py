c2904ba8aa1dee788e842df8db9e54f35d005ee7
Yoram Shamir <yoram-s@qualisystems.com>
2018-02-26 16:58:17 +0200

    fix requirements in setup

--- setup.py
+++ setup.py
@@ -5,8 +5,12 @@ try:
 except ImportError:
 	from distutils.core import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+install_requires = [r for r in required if r and r[0] != '#' and not r.startswith('git')]
+
 setup(name="pypacker27",
-	version="4.0.1",
+	version="4.0.2",
 	author="Michael Stahn",
 	author_email="michael.stahn.42@gmail.com",
 	url="https://github.com/mike01/pypacker/tree/python27",
@@ -30,5 +34,6 @@ setup(name="pypacker27",
 		"Programming Language :: Python :: Implementation :: CPython",
 		"Programming Language :: Python :: Implementation :: PyPy"
 	],
+    install_requires=install_requires,
     python_requires=">=2.7"
 )
